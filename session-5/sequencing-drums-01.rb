f08a239833b1b040895f1367f285083396a7c503
G. Martin Butz <mb@mkblog.org>
2015-12-09 08:52:45 +0100

    Added detailed comments.

--- session-5/sequencing-drums-01.rb
+++ session-5/sequencing-drums-01.rb
@@ -1,10 +1,19 @@
 # Sequencing Drum in Sonic Pi - Solution No. 1 (using 'tick')
-# TODO: Comment in detail
 
 use_debug false
 use_bpm 100
 
 load_samples [:drum_bass_hard, :drum_bass_soft, :drum_snare_hard, :drum_snare_soft, :drum_cymbal_closed]
+
+#
+# Set up lists for the rhythmic patterns; every number signifies a
+# (potential) duration, 0 is a rest and 1 is a hit
+# If a list contains 4 values these are quarter notes (8 = eighths
+# and 16 = sextenths)
+# Every list is linked to one sample and its parameters; thus the
+# lists are named according to whether they are on or off-beat-
+# patterns (for the off-beat the amplitude of the sample is lower than
+# for the on-beats)
 
 # bass patterns
 bass_pattern_on  = [ 1,0,0,0, 0,0,0,1, 0,0,0,0, 0,0,0,0 ]
@@ -20,12 +29,29 @@ hihat_pattern_of = [ 0,1,0,1, 0,1,1,0, 1,0,1,0, 0,1,1,0 ]
 hihat_pattern_op = [ 0,0,0,0, 0,0,0,1 ]
 
 
+# Uncomment if you would like to make a function which
+# you can call repeatingly; one call of this code represents
+# one bar; so if you call this function with "4.times do" you
+# will get a pattern of 4 bars
+
 #define :drums do
 
 # Play the Bass
 in_thread do
   16.times do
+    # "tick" is a handy function (also called "method") build in
+    # Sonic Pi; it generates a counter which increments every time
+    # it is called (1,2,3 and so forth); if you apply "tick" to a list
+    # within a loop, it will walk through the list; thus the first time
+    # we call it here it is raised from 0 to 1; at the sextenth repetition
+    # the value of tick is 15; the result is that this line checks 16 times
+    # whether the current value of the list "bass_pattern_on" is greater
+    # greater than 0; if it is the sample will be play and then sleep for
+    # 0.25 seconds, if not the code just sleeps and we have a rest.
     sample :drum_bass_hard if bass_pattern_on.tick > 0
+    # Sleep times have to be adjusted according to the number of
+    # values in your rhythm pattern: if your pattern contains 16 values
+    # the sleep time is 4 (quarter notes) / 16 or 1 quarter note / 4 = 0.25
     sleep 0.25
   end
 end
@@ -69,14 +95,19 @@ end
 
 in_thread do
   8.times do
+    # Here we 'close' the open hihat by clipping the sample after
+    # 17.5 % of its runtime
     sample :drum_cymbal_open, amp: 0.5, finish: 0.175 if hihat_pattern_op.tick > 0
     sleep 0.5
   end
 end
 
+# Uncomment if you would like to make a function which
+# you can call repeatingly; one call of this code represents
+# one bar; so if you call this function with "4.times do" you
+# will get a pattern of 4 bars
+
 #end #:drums
-
-
 
 # You can pack parts of or all drum elements into functions
 # Then it'll be easier to use it a a module within a song
@@ -86,4 +117,18 @@ comment do
     sleep 4
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- session-5/sequencing-drums-02.rb
+++ session-5/sequencing-drums-02.rb
@@ -1,5 +1,4 @@
 # Sequencing Drum in Sonic Pi - Solution No. 2
-# TODO: Comment in detail
 
 use_debug true
 use_bpm 100
@@ -7,6 +6,10 @@ use_bpm 100
 load_samples [:drum_bass_hard, :drum_bass_soft, :drum_snare_hard, :drum_snare_soft, :drum_cymbal_closed]
 
 # Return note duration based on the defined rythmic pattern
+# This functions is needs a list as a parameter (ptn); it will
+# check whether this contains 1, 2, 4, 8, 16 or 32 values and
+# return a sleep time; if the list contains other than the mentioned
+# number of values it returns an error message.
 define :get_dur do |ptn|
   if ptn.count == 1
     d = 4
@@ -26,22 +29,61 @@ define :get_dur do |ptn|
   return d
 end
 
+# This function plays a sample repeatedly with given parameters:
+# smpl  = name of the sample
+# amp   = volume
+# rate  = rate (velocity and pitch)
+# pan   = direction concerning the stereo panorama
+# start = start time (whole sample runs from 0 = start until
+#         1 = the sample length; thus 0.5 means, the sample
+#         start after 50% of its length)
+# fin   = end time of sample
+# ptn   = rhythmic pattern for sample repetition
+#
+# Note: the function is not bullit proof; it only works if you
+# provide all parameters even if you do not want to change the
+# default values; this is because it does make things easier
+# to understand. More elegant it would be (but also more complicated)
+# if we you'd only have to provide e.g. the parameter for amp if you
+# intended to deviate from the default value.
+# Also we could provide all the parameters in a list ... but let's
+# leave it like this to keep an easy abstraction level.
 
 define :play_sample do | smpl, amp, rate, pan, start, fin, ptn |
-  # take orders, what and how to play
 
-  # work out time measure
+  # Work out the sleep time
+  # You see: You can call a function to do some work from
+  # within another function; the return value of "get_dur" is
+  # handed over to the variable d
   d = get_dur ptn
 
+  # "ptn.each do" part of a native ruby block (ruby being the programming
+  # language, Sonic Pi is being made of). It is also a function or rather
+  # method which is 'called on' a list called ptn. It does similar things
+  # as e.g. the "tick" method in that it goes through each value of the list.
+  # It does this by implementing a counter "i" for each value of the list.
   ptn.each do |i|
+    # If the list "ptn" at the number i is not 0, then play the sample
+    # with the given parameters.
     if i != 0
       sample smpl, amp: amp, rate: rate, pan: pan, start: start, finish: fin
     end
+    # Then sleep for the time specified by the variable "d".
     sleep d
   end
 end
 
 # Patterns for Bass and Snare
+
+# You could define a rhythmic pattern as a list assigned to
+# a variable just as we did in sequencing-drums-01.rb.
+# Only this would be a local variable; you could not transfer
+# it from one buffer to another. But as long as you keep your
+# variables in one and the same file/buffer it is okay to do so.
+#
+# Following is an example how you can transfer variables between
+# files/buffers. Just wrap them with a function; once you eval this
+# code (= press run in Sonic Pi), you can use them in any buffer.
 
 define :get_bass_on_ptn do
   bass_on_ptn_1    = [1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0]
@@ -73,25 +115,39 @@ end
 
 # Sequence for Bass and Snare
 
+#
+# Here we define our 'instruments' and the overall structure e.g.
+# for one bar. We call the function "play_sample" with our defined
+# parameters. Thus we can e.g. control the amplitude or the rate
+# of the sample.
+#
+# Try to experiment with changing the rate in different patterns
+# You can also replace the rate value by an expression such as
+# rrand(0.5, 1.5) to make it change every time it is called.
+
 define :mod_bass_snare do
   in_thread do
     4.times do
       play_sample :drum_bass_hard, 1.0, 0.75, -0.2, 0, 1, get_bass_on_ptn
+      get_bass_on_ptn
     end
   end
   in_thread do
     4.times do
       play_sample :drum_bass_hard, 0.75, 0.5, -0.2, 0, 1, get_bass_of_ptn
+      get_bass_of_ptn
     end
   end
   in_thread do
     4.times do
       play_sample :drum_snare_hard, 0.75, 1.275, -0.1, 0, 1, get_snre_on_ptn
+      get_snre_on_ptn
     end
   end
   in_thread do
     4.times do
       play_sample :drum_snare_hard, 1, 1.3, -0.1, 0, 1, get_snre_of_ptn
+      get_snre_of_ptn
     end
   end
 end
